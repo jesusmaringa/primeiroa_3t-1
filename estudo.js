ae36af5f57d06a86b41fa0d3967ad3d1aacc6375
antoniogabriel23 <108940128+antoniogabriel23@users.noreply.github.com>
2022-10-19 08:19:52 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -39,3 +39,30 @@ function calcula(){
     let r = c *(1 + (j/100));
     document.write("resultado: " + r);
 }
+
+let op = "";
+function operacao(ope){
+  op = ope;
+}
+function calcule(){
+    let v1 = document.getElementById("v1").value;
+    let v2 = document.getElementById("v2").value;
+    let r =0;
+    
+    if(op == "+"){
+        r = Number(v1) + Number(v2);
+    }
+    if(op == "-"){
+        r = Number(v1) - Number(v2);
+    }
+    if(op == "*"){
+        r = Number(v1) * Number(v2);
+    }
+    if(op == "/"){
+        r = Number(v1) / Number(v2);
+    }
+
+
+    document.getElementById("resultado").innerHTML = r;
+
+}
